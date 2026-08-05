a6662a41b042e15b29e2a5cd5f992cd4d9ff7069
mnfds <daus2016.d2@gmail.com>
2026-08-05 19:00:31 +0800

    style: ubah warna button wa menjadi lebih gelap

--- resources/views/partials/landing/whatsapp-button.blade.php
+++ resources/views/partials/landing/whatsapp-button.blade.php
@@ -16,9 +16,9 @@
     </span>
 
     {{-- Tombol bulat --}}
-    <span class="relative flex items-center justify-center w-12 h-12 rounded-tl-[15px] rounded-ee-[15px] bg-[#25D366] text-ivory shadow-lg transition-transform duration-300 group-hover:scale-105 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gold">
+    <span class="relative flex items-center justify-center w-12 h-12 rounded-tl-[15px] rounded-ee-[15px] bg-[#1fb456] text-ivory shadow-lg transition-transform duration-300 group-hover:scale-105 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gold">
         {{-- Ping animation, subtle --}}
-        <span class="absolute inset-0 rounded-tl-[15px] rounded-ee-[15px] bg-[#25D366] animate-ping opacity-20"></span>
+        <span class="absolute inset-0 rounded-tl-[15px] rounded-ee-[15px] bg-[#1fb456] animate-ping opacity-20"></span>
 
         <svg viewBox="0 0 24 24" class="relative w-7 h-7" fill="currentColor">
             <path d="M12.04 2C6.58 2 2.13 6.45 2.13 11.91c0 1.75.46 3.45 1.32 4.95L2 22l5.29-1.39c1.44.79 3.06 1.2 4.71 1.2h.01c5.46 0 9.91-4.45 9.91-9.91C21.92 6.45 17.5 2 12.04 2zm5.8 14.02c-.24.68-1.4 1.3-1.93 1.38-.5.08-1.13.11-1.82-.11-.42-.13-.96-.31-1.65-.6-2.9-1.25-4.79-4.17-4.94-4.36-.14-.19-1.18-1.57-1.18-3 0-1.42.75-2.12 1.02-2.41.26-.28.58-.35.77-.35s.39 0 .56.01c.18.01.42-.07.65.5.24.58.82 2 .89 2.15.07.15.12.32.02.51-.09.19-.14.31-.28.48-.14.16-.29.36-.42.48-.14.13-.28.28-.12.55.16.28.72 1.19 1.54 1.93 1.06.95 1.95 1.24 2.23 1.38.28.13.44.11.6-.07.16-.18.7-.81.88-1.09.19-.28.37-.23.62-.14.26.1 1.63.77 1.91.91.28.14.47.21.54.33.07.12.07.68-.17 1.36z"/>
